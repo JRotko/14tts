716df28de544a1a7ec0ca4cd6dca03e6a461d178
JRotko <76393423+JRotko@users.noreply.github.com>
2022-03-10 09:42:56 +0200

    valmis

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
+import {useState} from 'react'
 
 export default function App() {
+  const [text, setText] = useState('')
+
+  const speak = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text}/>
+      <Button onPress={speak} title="Text to Speech"></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    marginBottom: 10,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
   },
 });
